711a5ff522b47bf4460fb0a6eb55b357cafb57bf
SAI30570SREE@Rachapudi <144793915+SAI30570SREERachapudi@users.noreply.github.com>
2025-11-16 22:39:50 +0530

    Extend application class for servlet initialization

--- OnlineTicketBookingBackend/src/main/java/com/vst/OnlineTicketBookingBackendApplication.java
+++ OnlineTicketBookingBackend/src/main/java/com/vst/OnlineTicketBookingBackendApplication.java
@@ -2,12 +2,19 @@ package com.vst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OnlineTicketBookingBackendApplication {
+public class OnlineTicketBookingBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineTicketBookingBackendApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(OnlineTicketBookingBackendApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineTicketBookingBackendApplication.class, args);
+    }
 }
+
