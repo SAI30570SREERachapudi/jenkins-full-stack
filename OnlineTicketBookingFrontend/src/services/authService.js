7a1ae5790bdfd91363bff2b8c0ef1cbf7dadcdbf
SAI30570SREERachapudi <saisreegithub@gmail.com>
2025-11-16 21:34:05 +0530

    Port changed to 9090

--- OnlineTicketBookingFrontend/src/services/authService.js
+++ OnlineTicketBookingFrontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8808/api/auth";
+const API_BASE_URL = "http://localhost:9090/api/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_BASE_URL}/login`, {
